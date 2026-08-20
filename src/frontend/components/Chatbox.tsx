9c0c92803edbd322770c4cd47914350074894af3
Abby Dise <abbydisedev@gmail.com>
2026-08-20 12:25:21 -0400

    added an auto-scrolling feature for new messages and decreased the message font to 18px

--- src/frontend/components/Chatbox.tsx
+++ src/frontend/components/Chatbox.tsx
@@ -1,13 +1,25 @@
 'use client'
 
 import type { Message } from '../app/page.tsx';
+import {Fragment, useEffect, useRef} from "react";
 
 export default function Chatbox ({messages} : {messages : Array<Message>}) {
+    const bottomRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        if (bottomRef.current) {
+            bottomRef.current.scrollIntoView({behavior: "smooth", block: "end"})
+        }
+    }, [messages])
+
     return (
-        messages.map((message, index) => (
-            <div key={index} className={message.origin === "user" ? "user-message" : "system-message"}>
-                <p>{message.content}</p>
-            </div>
-        ))
+        <Fragment>
+            {messages.map((message, index) => (
+                <div key={index} className={message.origin === "user" ? "user-message" : "system-message"}>
+                    <p>{message.content}</p>
+                </div>
+            ))}
+            <div ref={bottomRef} />
+        </Fragment>
     )
 }
